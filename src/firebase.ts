0a0a9eceff29c385843dd1b89a619634d5f00b8b
光山明希 <>
2021-01-24 11:08:12 +0900

    🎉

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+import * as firebase from "firebase/app";
+import "firebase/app";
+import "firebase/firestore";    //データーベースを使うのに必要なモジュール
+import "firebase/auth";         //認証機能を使うのに必要なモジュール
+
+// アプリケーションをイニシャライズアップする
+// 環境変数envを呼んできて各パラメーターに設定
+const firebaseApp = firebase.initializeApp({
+  apikey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectid: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+});
+
+// Reactのコンポーネントからデーターベースと認証モジュールを利用するためにエクスポート
+export const db = firebaseApp.firestore();
+export const auth = firebaseApp.auth();
