52cb72e80c109e7a651eb42de3b25b662dfd8293
Omitsu <a826.orhrp@gmail.com>
2021-01-24 12:05:38 +0900

    🎉

--- src/firebase.ts
+++ src/firebase.ts
@@ -18,3 +18,5 @@ const firebaseApp = firebase.initializeApp({
 // Reactのコンポーネントからデーターベースと認証モジュールを利用するためにエクスポート
 export const db = firebaseApp.firestore();
 export const auth = firebaseApp.auth();
+
+
