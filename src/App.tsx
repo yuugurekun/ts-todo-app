cb334d20a81055a2152665c06754c9e04a0253e9
Omitsu <a826.orhrp@gmail.com>
2021-02-01 23:08:29 +0900

    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { FormControl, List, TextField } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import styles from "./App.module.css";
 import { db } from "./firebase";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
+import TaskItem from "./TaskItem";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  field: {
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  list: {
+    margin: "auto",
+    width: "40%",
+  },
+});
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [input, setInput] = useState("");
+  const classes = useStyles();
+
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
       setTasks(
@@ -13,11 +31,36 @@ const App: React.FC = () => {
     return () => unSub();
   }, []);
 
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    db.collection("tasks").add({ title: input });
+    setInput("");
+  };
+
   return (
-    <div className="App">
-      {tasks.map((task) => (
-        <h3 key={task.id}>{task.title}</h3>
-      ))}
+    <div className={styles.app__root}>
+      <h1>Todo App by React/Firebase</h1>
+      <br />
+      <FormControl>
+        <TextField
+          className={classes.field}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label="New task ?"
+          value={input}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInput(e.target.value)
+          }
+        />
+      </FormControl>
+      <button className={styles.app__icon} disabled={!input} onClick={newTask}>
+        <AddToPhotosIcon />
+      </button>
+      <List className={classes.list}>
+        {tasks.map((task) => (
+          <TaskItem key={task.id} id={task.id} title={task.title} />
+        ))}
+      </List>
     </div>
   );
 };
